e9017e405f67efac8224be6cb1a080b3ef29efa4
ngo275 <ngo38114@gmail.com>
2018-05-25 06:47:57 +0900

    Initial Commit

---
+++ PDFKitSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PDFKitSample
+//
+//  Created by ShuichiNagao on 2018/05/25.
+//  Copyright © 2018 Shuichi Nagao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
